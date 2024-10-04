635674876049122c5b2a028ad87a08ed45079bac
yordan9 <dancho00011997@abv.bg>
2024-10-04 09:41:24 +0300

    Update Dev-B

--- OptionsManager.cs
+++ OptionsManager.cs
@@ -7,7 +7,9 @@ namespace GitExercise
         public static string[] OptionsList = {
             "a - Add",
             "s - Subtract",
-            "m - Multiply"
+            "m - Multiply",
+            "dr - Divide Remainder",
+            "ex - Exit"
         };
 
         public static void Add(double a, double b)
@@ -23,6 +25,11 @@ namespace GitExercise
         public static void Subtract(double a, double b)
         {
             Console.WriteLine($"{a} - {b} = {a - b}");
+        }
+
+        public static void DivideRemainder(double a, double b)
+        {
+            Console.WriteLine($"{a} % {b} = {a % b}");
         }
     }
 }
